e9373906514ba9e1e57d4c42bd73493ff90ad05e
sompasongvongthavone <sompasongvongthavone@192.168.100.204>
2021-12-20 10:12:17 +0700

    First init

--- src/finalexam/Grade.java
+++ src/finalexam/Grade.java
@@ -33,11 +33,11 @@ public class Grade extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 493, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 213, Short.MAX_VALUE)
         );
 
         pack();
